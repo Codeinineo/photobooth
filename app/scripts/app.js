032958ff2b5860e1fc377da7b5971eb6099c4c6d
Maarten Anckaert <maarten.anckaert@icapps.com>
2016-08-02 19:05:56 +0200

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name photoboothApp
+ * @description
+ * # photoboothApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('photoboothApp', ['webcam']);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name photoboothApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the photoboothApp
+ */
+angular.module('photoboothApp')
+  .controller('MainCtrl', function () {
+  });
